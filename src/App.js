9a4604adbde55be40fb2749384b04b7d156eace1
[nemanja s.] <nemanja94sesa@gmail.com>
2024-08-11 05:52:23 -0700

    nav, header, main, footer files added & imported to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
+import "./App.css";
+import Header from "./header";
+import Nav from "./nav";
+import Main from "./main";
+import Footer from "./footer";
 
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+    <>
+      <Nav />
+      <Header />
+      <Main />
+      <Footer />
+    </>
   );
 }
 
---
+++ src/nav.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <nav className="Nav">
+      <ul>
+        <li>Home</li>
+        <li>Menu</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
