559102cb311409ad50469e2451309450bd4612b6
Nemanja Sesum <nemanja94sesa@gmail.com>
2024-08-11 18:00:39 -0700

    positioned nav & header elements

--- src/header.js
+++ src/header.js
@@ -1,7 +1,20 @@
 import React from "react";
-
+import "./header.css"
+import HeaderImg from "./assets/restauranfood.jpg"
 const Header = () => {
-  return <header className="header">Header component</header>;
+  return (
+    <header className="headerWrapper">
+      <div className="headerLeft">
+        <h1>Little Lemon</h1>
+        <h3>Chicago</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, corrupti voluptatibus voluptatum est quidem repellendus ducimus eligendi aliquid inventore sunt vitae praesentium aperiam dolor doloremque dolore excepturi temporibus, nobis totam.</p>
+        <button>Reserve a Table</button>
+      </div>
+      <div className="headerRight">
+        <img src={HeaderImg} alt="Restaurant"></img>
+      </div>
+    </header>
+  )
 };
 
 export default Header;
--- src/nav.js
+++ src/nav.js
@@ -1,13 +1,18 @@
 import React from "react";
+import "./nav.css"
+import {ReactComponent as Logo} from "./assets/Logo .svg"
 
 const Nav = () => {
   return (
-    <nav className="Nav">
+    <nav className="Navbar">
+      <Logo />
       <ul>
-        <li>Home</li>
-        <li>Menu</li>
-        <li>About</li>
-        <li>Contact</li>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#menu">Menu</a></li>
+        <li><a href="#reservations">Reservations</a></li>
+        <li><a href="#onlineOrder">Order Online</a></li>
+        <li><a href="#login">Login</a></li>
       </ul>
     </nav>
   );
