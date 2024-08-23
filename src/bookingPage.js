992c38e169bc4ed5e5975f1bbccd86c0311fd56a
Nemanja Sesum <nemanja94sesa@gmail.com>
2024-08-22 22:02:52 -0700

    added useState hooks to bookingPage, next useReducer

--- src/bookingPage.js
+++ src/bookingPage.js
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Nav from "./nav";
 import Footer from "./footer";
 import "./bookingPage.css";
 
 const BookingPage = () => {
+  const [resDate, setResDate] = useState("")
+  const [resTime, setResTime] = useState("")
+  const [guests, setGuests] = useState("")
+  const [occasion, setOccasion] = useState("occasion")
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form submitted!");
+  }
+
   return (
     <div>
       <Nav />
       <div className="formWrapper">
-        <form>
-          <label for="res-date">Choose date</label>
-          <input type="date" id="res-date"></input>
-          <label for="res-time">Choose time</label>
-          <select id="res-time ">
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="res-date">Choose date</label>
+          <input value={resDate} onChange={(e) => {
+            setResDate(e.target.value);
+          }} type="date" id="res-date"></input>
+          <label htmlFor="res-time">Choose time</label>
+          <select value={resTime} onChange={(e) => setResTime(e.target.value)} id="res-time ">
             <option>17:00</option>
             <option>18:00</option>
             <option>19:00</option>
@@ -20,16 +33,17 @@ const BookingPage = () => {
             <option>21:00</option>
             <option>22:00</option>
           </select>
-          <label for="guests">Number of guests</label>
+          <label htmlFor="guests">Number of guests</label>
           <input
+            value={guests} onChange={(e) => setGuests(e.target.value)}
             type="number"
             placeholder="1"
             min="1"
             max="10"
             id="guests"
           ></input>
-          <label for="occasion">Occasion</label>
-          <select id="occasion">
+          <label htmlFor="occasion">Occasion</label>
+          <select value={occasion} onChange={(e) => setOccasion(e.target.value)} id="occasion">
             <option>Birthday</option>
             <option>Anniversary</option>
           </select>
