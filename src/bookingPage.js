e08661cdbcc52e4d69218537adeb91aebb705aac
[nemanja s.] <nemanja94sesa@gmail.com>
2024-08-23 12:28:16 -0700

    added form.js file, separated bookingForm from Booking page

--- src/bookingPage.js
+++ src/bookingPage.js
@@ -1,55 +1,13 @@
-import React, { useState } from "react";
 import Nav from "./nav";
+import BookingForm from "./form";
 import Footer from "./footer";
 import "./bookingPage.css";
 
 const BookingPage = () => {
-  const [resDate, setResDate] = useState("")
-  const [resTime, setResTime] = useState("")
-  const [guests, setGuests] = useState("")
-  const [occasion, setOccasion] = useState("occasion")
-
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("Form submitted!");
-  }
-
   return (
     <div>
       <Nav />
-      <div className="formWrapper">
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="res-date">Choose date</label>
-          <input value={resDate} onChange={(e) => {
-            setResDate(e.target.value);
-          }} type="date" id="res-date"></input>
-          <label htmlFor="res-time">Choose time</label>
-          <select value={resTime} onChange={(e) => setResTime(e.target.value)} id="res-time ">
-            <option>17:00</option>
-            <option>18:00</option>
-            <option>19:00</option>
-            <option>20:00</option>
-            <option>21:00</option>
-            <option>22:00</option>
-          </select>
-          <label htmlFor="guests">Number of guests</label>
-          <input
-            value={guests} onChange={(e) => setGuests(e.target.value)}
-            type="number"
-            placeholder="1"
-            min="1"
-            max="10"
-            id="guests"
-          ></input>
-          <label htmlFor="occasion">Occasion</label>
-          <select value={occasion} onChange={(e) => setOccasion(e.target.value)} id="occasion">
-            <option>Birthday</option>
-            <option>Anniversary</option>
-          </select>
-          <input type="submit" value="Make Your reservation"></input>
-        </form>
-      </div>
+      <BookingForm />
       <Footer />
     </div>
   );
---
+++ src/form.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import "./bookingPage.css";
+
+const BookingForm = () => {
+  const [resDate, setResDate] = useState("");
+  const [resTime, setResTime] = useState("");
+  const [guests, setGuests] = useState("");
+  const [occasion, setOccasion] = useState("occasion");
+
+  // const handleChange = (e) => {
+  //   const [name, value] = e.target;
+
+  // }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form submitted!");
+  };
+
+  return (
+    <div className="formWrapper">
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="res-date">Choose date</label>
+        <input
+          value={resDate}
+          onChange={(e) => {
+            setResDate(e.target.value);
+          }}
+          type="date"
+          id="res-date"
+        ></input>
+        <label htmlFor="res-time">Choose time</label>
+        <select
+          value={resTime}
+          onChange={(e) => setResTime(e.target.value)}
+          id="res-time "
+        >
+          <option>17:00</option>
+          <option>18:00</option>
+          <option>19:00</option>
+          <option>20:00</option>
+          <option>21:00</option>
+          <option>22:00</option>
+        </select>
+        <label htmlFor="guests">Number of guests</label>
+        <input
+          value={guests}
+          onChange={(e) => setGuests(e.target.value)}
+          type="number"
+          placeholder="1"
+          min="1"
+          max="10"
+          id="guests"
+        ></input>
+        <label htmlFor="occasion">Occasion</label>
+        <select
+          value={occasion}
+          onChange={(e) => setOccasion(e.target.value)}
+          id="occasion"
+        >
+          <option>Birthday</option>
+          <option>Anniversary</option>
+        </select>
+        <input type="submit" value="Make Your reservation"></input>
+      </form>
+    </div>
+  );
+};
+export default BookingForm;
